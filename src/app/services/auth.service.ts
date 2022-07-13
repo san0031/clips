1483830347d333a96d422d12968f58e737c1d74e
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-12 21:28:21 -0500

    Upto lesson 164

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,8 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import IUser from 'src/app/models/user.model';
 
 @Injectable({
@@ -11,8 +13,14 @@ import IUser from 'src/app/models/user.model';
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
+  private IsAuthenticated$: Observable<boolean>
+
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users');
+    this.IsAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+
+    )
   }
 
   public async createUser(userData: IUser) {
