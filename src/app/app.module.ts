38930cf2affc5807f3642c008c79592882d8257b
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-13 00:21:40 -0500

    Upto lesson 179

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { EmailTaken } from './user/validators/email-taken';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
