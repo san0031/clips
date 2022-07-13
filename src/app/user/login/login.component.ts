982e5b1dd9cb5d02cad8e4a108f83cf6c0325715
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-12 22:25:01 -0500

    upto lesson 168

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -15,15 +16,29 @@ export class LoginComponent implements OnInit {
   alertMsg = 'Please wait! Login in Progress.';
   alertColor = 'blue';
 
-  constructor() {}
-
+  constructor(private auth: AngularFireAuth) {}
+  inSubmission = false;
   ngOnInit(): void {}
 
-  login() {
+  async login() {
     this.showAlert = true;
     this.alertMsg = 'Please wait! Login in Progress.';
     this.alertColor = 'blue';
+    this.inSubmission = true;
 
-    //console.log('login  called ;Form submitted');
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (e) {
+      this.alertMsg = 'Unexpected error occurred. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      console.log(e);
+      return;
+    }
+    this.alertMsg = 'Login successful';
+    this.alertColor = 'green';
   }
 }
