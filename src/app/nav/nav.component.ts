a9fe4254e6d0a86745e1ec9c9353913efba85479
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-16 23:24:32 -0500

    Upto lesson 188

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +12,7 @@ export class NavComponent implements OnInit {
   constructor(
     public modal: ModalService,
     public auth: AuthService,
-    public afAuth: AngularFireAuth
+ 
   ) {}
 
   ngOnInit(): void {}
@@ -24,8 +24,5 @@ export class NavComponent implements OnInit {
     //console.log('In openModal Nav Component Event value is', $event);
   }
 
-  async logout($event: Event) {
-    $event.preventDefault();
-    await this.afAuth.signOut();
-  }
+ 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import {
 import { Observable } from 'rxjs';
 import { delay, map } from 'rxjs/operators';
 import IUser from 'src/app/models/user.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,11 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
@@ -45,5 +50,14 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name,
     });
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+
+    await this.auth.signOut();
+    await this.router.navigateByUrl('/');
   }
 }
