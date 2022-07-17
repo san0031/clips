30c9c4dc02072ba6dcada156eb4bb51accd858d4
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-17 00:19:23 -0500

    Upto lesson 193

---
+++ src/app/clip/clip.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-clip',
+  templateUrl: './clip.component.html',
+  styleUrls: ['./clip.component.css'],
+})
+export class ClipComponent implements OnInit {
+  id = '';
+  constructor(public route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+  }
+}
