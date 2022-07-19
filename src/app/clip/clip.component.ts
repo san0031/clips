d1f21f4f6e5ee4d62dd111fdcb56943961d64b76
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-18 22:27:53 -0500

    Upto lesson 194

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,5 +1,6 @@
+import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clip',
@@ -11,6 +12,9 @@ export class ClipComponent implements OnInit {
   constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params['id'];
+    //this.id = this.route.snapshot.params['id'];
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+    });
   }
 }
