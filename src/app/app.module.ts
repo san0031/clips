ace51da197bb8f4928c003c474719b9e1ea2e237
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-18 23:08:46 -0500

    Upto lesson 199

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AngularFireAuthModule,
     AngularFirestoreModule,
     VideoModule,
-    AppRoutingModule,
+    AppRoutingModule, 
   ],
   providers: [],
   bootstrap: [AppComponent],
