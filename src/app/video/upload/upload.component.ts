0410307b80335e9c24e5b16026608d1ea3e822b0
Srividya Ananthasubramoney <san0031@gmail.com>
2022-08-08 12:16:57 -0500

    Upto  lesson 216

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -44,8 +44,11 @@ export class UploadComponent implements OnInit {
   });
 
   storeFile($event: Event) {
+    //console.log($event);
     this.isDragover = false;
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.file = ($event as DragEvent).dataTransfer
+      ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : ($event.target as HTMLInputElement).files?.item(0) ?? null;
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
